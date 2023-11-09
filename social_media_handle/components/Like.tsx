a88ff9362247a634ddfada7f74881ac065438c56
Gopi venkat <67084912+0gopi0@users.noreply.github.com>
2023-11-09 14:26:25 +0530

    Update Like.tsx

--- social_media_handle/components/Like.tsx
+++ social_media_handle/components/Like.tsx
@@ -1,10 +1,17 @@
 import { FcLike } from 'react-icons/fc';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { useState } from 'react';
+
 function Like() {
   const [one, two] = useState(true);
-  if (one) return <AiOutlineHeart size="30" onClick={() => two(false)} />;
-  return <FcLike  size="30" onClick={() => two(true)} />;
+  const like_var =
+    one === true ? (
+      <AiOutlineHeart size="30" onClick={() => two(false)} />
+    ) : (
+      <FcLike size="30" onClick={() => two(true)} />
+    );
+
+  return like_var;
 }
 
 export default Like;
